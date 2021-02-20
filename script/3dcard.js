962cf16f730e6a43c63ad71aa08f37dfa8dd1d8d
Breno César <44047399+EIleven@users.noreply.github.com>
2021-02-20 17:38:16 -0300

    att-card

--- script/3dcard.js
+++ script/3dcard.js
@@ -1,7 +1,7 @@
 let card = document.querySelector('.card');
 
 document.addEventListener('pointermove', function(e) {
-    let xAxis = (window.innerWidth / 2 - e.pageX) / 10;
-    let yAxis = (window.innerHeight / 2 - e.pageY) / 5;
+    let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+    let yAxis = (window.innerHeight / 2 - e.pageY) / 15;
     card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 });
